cd7972bc405bd8516ae6bcf26b019b189ef4d00e
Laurent Leseigneur <laurent.leseigneur@bonitasoft.com>
2019-06-17 15:20:16 +0200

    e2e tests with version

--- e2e.sh
+++ e2e.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
-./gradlew clean check distZip
-unzip -q -o build/distributions/bonita-saml-metadata.zip -d build/distributions
 
-cp e2e/keycloack-saml.xml build/distributions/bonita-saml-metadata/keycloak-example.xml
-cp e2e/application-signed.properties build/distributions/bonita-saml-metadata/application-signed.properties
-cp e2e/application-not-signed.properties build/distributions/bonita-saml-metadata/application-not-signed.properties
-cd build/distributions/bonita-saml-metadata
+./gradlew clean check distZip
+
+#get current version
+./gradlew -q currentVersion > build/version.txt
+jarVersion=`head -1 build/version.txt`
+
+unzip -q -o "build/distributions/bonita-saml-metadata-${jarVersion}".zip -d build/distributions
+
+cp e2e/keycloack-saml.xml build/distributions/bonita-saml-metadata-${jarVersion}/keycloak-example.xml
+cp e2e/application-signed.properties build/distributions/bonita-saml-metadata-${jarVersion}/application-signed.properties
+cp e2e/application-not-signed.properties build/distributions/bonita-saml-metadata-${jarVersion}/application-not-signed.properties
+cd build/distributions/bonita-saml-metadata-${jarVersion}
 
 pwd
 ls -l
